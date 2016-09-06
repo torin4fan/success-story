492e0a2f4b715e8ea3a909f94e88482a460310a6
Maksym Kolisnyk <Maksym_Kolisnyk@epam.com>
2016-09-06 21:00:36 +0300

    v1

---
+++ webpack.config.js
@@ -0,0 +1,174 @@
+// Helper: root() is defined at the bottom
+const path = require('path');
+const webpack = require('webpack');
+
+// Webpack Plugins
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+
+/**
+ * Env
+ * Get npm lifecycle event to identify the environment
+ */
+var ENV = process.env.npm_lifecycle_event;
+var isTest = ENV === 'test' || ENV === 'test-watch';
+var isProd = ENV === 'build';
+
+module.exports = function makeWebpackConfig() {
+    /**
+     * Config
+     * Reference: http://webpack.github.io/docs/configuration.html
+     * This is the object where all configuration gets set
+     */
+    var config = {};
+
+    /**
+     * Devtool
+     * Reference: http://webpack.github.io/docs/configuration.html#devtool
+     * Type of sourcemap to use per build type
+     */
+    if (isProd) {
+        config.devtool = 'source-map';
+    } else {
+        config.devtool = 'eval-source-map';
+    }
+
+    // add debug messages
+    config.debug = !isProd || !isTest;
+
+    /**
+     * Entry
+     * Reference: http://webpack.github.io/docs/configuration.html#entry
+     */
+    config.entry = isTest ? {} : {
+
+    };
+
+    /**
+     * Output
+     * Reference: http://webpack.github.io/docs/configuration.html#output
+     */
+    config.output = isTest ? {} : {
+
+    };
+
+    /**
+     * Resolve
+     * Reference: http://webpack.github.io/docs/configuration.html#resolve
+     */
+    config.resolve = {
+
+    };
+
+    /**
+     * Loaders
+     * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+     * List: http://webpack.github.io/docs/list-of-loaders.html
+     * This handles most of the magic responsible for converting modules
+     */
+    config.module = {
+        preLoaders: isTest ? [] : [{test: /\.ts$/, loader: 'tslint'}],
+        loaders: [
+
+        ],
+        postLoaders: []
+    };
+
+    if (isTest) {
+
+    }
+
+    /**
+     * Plugins
+     * Reference: http://webpack.github.io/docs/configuration.html#plugins
+     * List: http://webpack.github.io/docs/list-of-plugins.html
+     */
+    config.plugins = [
+        // Define env variables to help with builds
+        // Reference: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+        new webpack.DefinePlugin({
+            // Environment helpers
+            'process.env': {
+                ENV: JSON.stringify(ENV)
+            }
+        })
+    ];
+
+    if (!isTest && !isProd) {
+        config.plugins.push(new DashboardPlugin());
+    }
+
+    if (!isTest) {
+        config.plugins.push(
+
+        );
+    }
+
+    // Add build specific plugins
+    if (isProd) {
+        config.plugins.push(
+            // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+            // Only emit files when there are no errors
+            new webpack.NoErrorsPlugin(),
+
+            // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+            // Dedupe modules in the output
+            new webpack.optimize.DedupePlugin(),
+
+            // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+            // Minify all javascript, switch loaders to minimizing mode
+            new webpack.optimize.UglifyJsPlugin({mangle: { keep_fnames: true }}),
+
+            // Copy assets from the public folder
+            // Reference: https://github.com/kevlened/copy-webpack-plugin
+            new CopyWebpackPlugin([{
+                from: root('src/public')
+            }])
+        );
+    }
+
+    /**
+     * PostCSS
+     * Reference: https://github.com/postcss/autoprefixer-core
+     * Add vendor prefixes to your css
+     */
+    config.postcss = [
+        autoprefixer({
+            browsers: ['last 2 version']
+        })
+    ];
+
+    /**
+     * Sass
+     * Reference: https://github.com/jtangelder/sass-loader
+     * Transforms .scss files to .css
+     */
+    config.sassLoader = {
+        //includePaths: [path.resolve(__dirname, "node_modules/foundation-sites/scss")]
+    };
+
+
+    /**
+     * Dev server configuration
+     * Reference: http://webpack.github.io/docs/configuration.html#devserver
+     * Reference: http://webpack.github.io/docs/webpack-dev-server.html
+     */
+    config.devServer = {
+        contentBase: './src/public',
+        historyApiFallback: true,
+        quiet: true,
+        stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
+    };
+
+    return config;
+}();
+
+// Helper functions
+function root(args) {
+    args = Array.prototype.slice.call(arguments, 0);
+    return path.join.apply(path, [__dirname].concat(args));
+}
